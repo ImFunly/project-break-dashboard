106eecc2acf3c31b4fb1cbb90bda9e11f55f0b9c
Samuel Pérez <samux7421x@hotmail.com>
2024-04-10 22:47:55 +0200

    tiempo

--- js/tiempo.js
+++ js/tiempo.js
@@ -1,8 +1,8 @@
-const insertClima = document.getElementById("insertClime");
+const insertClime = document.getElementById("insertClime");
 
-const apiKey = "f75c6c93612549e8911142624240804";
+const apiKey = "bc614f989cc648bab74201418241004";
 const city = "Almeria";
-
+let date = new Date();
 const getClime = async () => {
   try {
     const respuesta = await fetch(
@@ -12,7 +12,44 @@ const getClime = async () => {
       throw new Error("Error al obtener datos del clima: " + respuesta.status);
     }
     const data = await respuesta.json();
-    console.log(data);
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+    insertClime.appendChild(container);
+
+    const location = document.createElement("div");
+    location.classList.add("location");
+    location.innerHTML = `${data.location.name} / ${data.location.country}`;
+    container.appendChild(location);
+
+    const currentWeather = document.createElement("div");
+    currentWeather.classList.add("current");
+    currentWeather.innerHTML = `
+      <div class="currentyellow">${data.current.condition.text}</div>
+      <img src="https:${data.current.condition.icon}" alt="Weather Icon"> 
+      <div>${data.current.temp_c}°C</div>
+      <div>Precipitaciones: ${data.current.precip_mm}% Humedad: ${data.current.humidity}% Viento: ${data.current.wind_kph} km/h</div>
+    `;
+    location.appendChild(currentWeather);
+
+    const forecastContainer = document.createElement("div");
+    forecastContainer.classList.add("forecastContainer");
+    container.appendChild(forecastContainer);
+    const dataForecast = data.forecast.forecastday;
+    dataForecast.forEach((item) => {
+      item.hour.forEach((hour) => {
+        const currentHours = document.createElement("div");
+        const time = hour.time.match(/([0-1][0-9]|2[0-3]):([0-5][0-9])/)[0];
+        currentHours.innerHTML = `<p>${time}</p>`;
+        forecastContainer.appendChild(currentHours);
+        const img = document.createElement("img");
+        img.src = `https:${hour.condition.icon}`;
+        forecastContainer.appendChild(img);
+        const grados = document.createElement("div");
+        grados.innerHTML = `<p>${hour.temp_c}°C</p>`;
+        forecastContainer.appendChild(grados);
+      });
+    });
   } catch (error) {
     console.error("Ha habido un error:", error);
   }
